6d62b8cd80dd9d329c6e0f04c3c0168edbd53ead
Eric Lin <ericlin.su@gmail.com>
2024-11-24 13:47:38 -0500

    Initial commit do not merge

---
+++ Starter Application/app/src/main/java/com/example/b07demosummer2024/EcoTrackerFragment.java
@@ -0,0 +1,36 @@
+package com.example.b07demosummer2024;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class EcoTrackerFragment extends Fragment {
+
+    private Button open_tracker;
+    private TextView daily_emission;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_eco_tracker, container, false);
+
+        daily_emission = view.findViewById(R.id.emissionsValue);
+
+        return view;
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+}
