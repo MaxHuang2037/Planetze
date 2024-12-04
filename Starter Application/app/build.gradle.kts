ac9551e07542bfa76711fab8b8fcc64ccee3b0d5
Arick-mux <137026734+Arick-mux@users.noreply.github.com>
2024-12-03 20:59:52 -0500

    the unit testing for login branch
    test cases here please

--- Starter Application/app/build.gradle.kts
+++ Starter Application/app/build.gradle.kts
@@ -30,24 +30,25 @@ android {
 }
 
 dependencies {
-    // Import the BoM for the Firebase platform
+    // Firebase platform BoM
     implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
 
-    // Add the dependency for the Firebase Authentication library
-    // When using the BoM, you don't specify versions in Firebase library dependencies
+    // Firebase dependencies
     implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
 
-
-
+    // Android libraries
     implementation(libs.mpandroidchart)
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.firestore)
-    testImplementation(libs.junit)
+
+    // Testing dependencies
+    testImplementation("junit:junit:4.13.2") // JUnit for unit testing
+    testImplementation("org.mockito:mockito-core:4.0.0") // Mockito for mocking
+    testImplementation("androidx.arch.core:core-testing:2.1.0") // AndroidX testing core
+
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
